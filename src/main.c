0089b73d27680f23c823dd5186fe95255a37c52e
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-02-11 11:36:39 -0500

    Driver program testing each function as it is built

--- src/main.c
+++ src/main.c
@@ -11,14 +11,18 @@ int main(void)
 	enqueue(mylist, "one");
         enqueue(mylist, "two");
         enqueue(mylist, "three");
-	printf("Count of the links: %d\n", mylist->count);
+	printf("Count of the links: %d\n\n", mylist->count);
 	
         print_list(mylist);
         node * mynode = pop(mylist);
-        printf("Node Popped: %s\n", mynode->word);
+        printf("Node Popped: %s\n\n", mynode->word);
         print_list(mylist);
         peek(mylist);
         node * temp2 = dequeue(mylist);
+        printf("Node Dequeued: %s\n\n", temp2->word);
         peek(mylist);
         printf("Size of list: %d\n", sizeoflist(mylist));
+        push(mylist, "zero");
+        print_list(mylist);
+        printf("The Tail: %s", mylist->tail->word);
 }
