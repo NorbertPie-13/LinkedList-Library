de2ec7e2ee12e513f553f68a96e932a9cc029238
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-02-14 08:41:59 -0500

    change driver program will redefine driver in driver branch

--- src/main.c
+++ src/main.c
@@ -11,21 +11,32 @@ int main(void)
 	enqueue(mylist, "one");
         enqueue(mylist, "two");
         enqueue(mylist, "three");
+
 	printf("Count of the links: %d\n\n", mylist->count);
 	
         print_list(mylist);
+
         node * mynode = pop(mylist);
-        free(mynode);
         printf("Node Popped: %s\n\n", mynode->word);
+        free(mynode);
+        
         print_list(mylist);
         peek(mylist);
+
         node * temp2 = dequeue(mylist);
-        free(temp2);
         printf("Node Dequeued: %s\n\n", temp2->word);
+        free(temp2);
+
+
+        
         peek(mylist);
         printf("Size of list: %d\n", sizeoflist(mylist));
         push(mylist, "zero");
+        push(mylist, "five");
+        push(mylist, "six");
+
         print_list(mylist);
         printf("The Tail: %s", mylist->tail->word);
+
         destroy_list(mylist);
 }
