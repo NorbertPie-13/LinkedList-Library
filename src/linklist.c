aa405533ee0c5595d4ae5e68d1ae11e148da9056
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-06-08 07:48:03 -0400

    Change Search function to take void pointer and add comments

--- src/linklist.c
+++ src/linklist.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include"linklist.h"
+
 
 /*Create links makes a linked list with no nodes. It holds the properties
 of a queue.  Insert will instead create the individual node when inserting.*/
-linklist *create_links(void)
+linklist *create_links(del_f delete)
 {				// Save linkedlist onto the heap setting everthing to 0
 	linklist *linkedlist = calloc(1, sizeof(*linkedlist));
 	if (linkedlist == NULL) {
 		fprintf(stderr, "Cannout create LinkedList struct\n");
 		return NULL;
 	}
+	linkedlist->delete = delete;
 	return linkedlist;
 }
 
-static node *create_node(char *word)
+static node *create_node(void *data)
 {
-	if (NULL == word) {
+	if (NULL == data) {
 		fprintf(stderr, "Cannot create node with an invalid pointer\n");
 		return NULL;
 	}
@@ -26,20 +29,20 @@ static node *create_node(char *word)
 		fprintf(stderr, "Could not create node containing");
 		return NULL;
 	}
-	// Assign the node's word to word
-	mynode->word = word;
+	mynode->data = data;
+
 	return mynode;
 }
 
 /* Create an individual node that assigns the word passed into it, as the word in the node.
 The node will be inserted at the tail of all the structures.*/
-linklist *enqueue(linklist * mylist, char *word)
+linklist *enqueue(linklist * mylist, void *data)
 {
 	if (NULL == mylist) {
 		fprintf(stderr, "Invalid pointer to link list in enqueue\n");
 		return NULL;
 	}
-	node *mynode = create_node(word);
+	node *mynode = create_node(data);
 	// If there is nothing in the queue, then head will be empty
 	if (mylist->head == NULL) {
 		// assign head to my node
@@ -62,7 +65,7 @@ linklist *enqueue(linklist * mylist, char *word)
 }
 
 /* pop returns the first object that is at the head and moves the head forward*/
-node *pop(linklist * my_list)
+void *pop(linklist * my_list)
 {
 	// check the pointer
 	if (NULL == my_list) {
@@ -71,6 +74,10 @@ node *pop(linklist * my_list)
 	}
 	// make a temporary value to hold head
 	node *temphead = my_list->head;
+	if (NULL == temphead) {
+		fprintf(stderr, "Head is empty.\n");
+		return NULL;
+	}
 	// assign head to the temporary's value next pointer
 	my_list->head = temphead->next;
 	// reduce the count
@@ -79,22 +86,28 @@ node *pop(linklist * my_list)
 	temphead->next = NULL;
 
 	//return temporary structure
-	return temphead;
+	void *data = temphead->data;
+	if (NULL == temphead->data) {
+		fprintf(stderr, "Head data is empty.\n");
+		return NULL;
+	}
+	free(temphead);
+	return data;
 
 }
 
-// Print list prints all objects that are currently in the list
 void print_list(linklist * mylist)
-{				//Check the pointer exists
-	if (!mylist) {
+{				
+	if (!mylist) //Check to see that mylist exists
+    {
 		return;
 	}
-	// set a temp struct to the head
-	node *temp = mylist->head;
-	// while temp exists,
-	while (temp) {
+	
+	node *temp = mylist->head; // create a temporary node structure of the head
+	
+	while (temp) // while a node structure exists
+    {
 		// print what temp is and assign to next
-		printf("%s -> ", temp->word);
 		temp = temp->next;
 	}
 	printf("\n");
@@ -102,85 +115,143 @@ void print_list(linklist * mylist)
 
 /* Peek is a void function that allows you to see what the head holds,
 and if there is a next value then it will show that value*/
-void peek(linklist * mylist)
+void *peek(linklist * mylist)
 {
 	// check the pointer
-	if (NULL == mylist) {
+	if (NULL == mylist)
+    {
 		printf("Cannot peek, nothing in list\n");
-		return;
+		return NULL;
 	}
 	// print the current word
-	if (NULL != mylist->head) {
-		printf("Current Head: %s\n", mylist->head->word);
+	if (NULL != mylist->head)
+    {
+		return mylist->head;
 	} else {
-		return;
+		return NULL;
 	}
 
-	// if there is a next, print that too!
-	if (NULL != mylist->head->next) {
-		printf("Next Node: %s\n", mylist->head->next->word);
-	}
 }
 
 /*Dequeue behaves exactly like pop*/
-node *dequeue(linklist * mylist)
+void *dequeue(linklist * mylist)
 {
-	return pop(mylist);
+	return pop(mylist); // call pop and return the item that was removed.
 }
 
 /* Return size of my list*/
 int sizeoflist(linklist * mylist)
 {
-	return mylist->count;
+	return mylist->count; // return the count of the elements in the LinkList
 }
 
 /*create a node and insert at the head of the queue. Returns void*/
-void push(linklist * mylist, char *word)
+void push(linklist * mylist, void *word)
 {
 	if (NULL == mylist || NULL == word) {
 		fprintf(stderr, "Cannnot push, invalid pointer provided\n");
 		return;
 	}
-	// make a node data structure
-	node *node_inserted = create_node(word);
-	// set the next of the node being inserted to the current head
-	node_inserted->next = mylist->head;
-	// set the head to the node inserted
-	mylist->head = node_inserted;
-	// increase the count.
-	mylist->count++;
+	node *node_inserted = create_node(word); // create a node data structure by calling create node
+	node_inserted->next = mylist->head; // set the next of the node being inserted to the current head
+	mylist->head = node_inserted; // set the head to the node inserted
+	mylist->count++; 	// increase the count of the linklist
 }
 
-static void destroy_node(node * mynode)
-{
-	if (NULL == mynode) {
-		fprintf(stderr, "Cannot destroy NULL value\n");
-		return;
-	}
-	// if next exists, then free next
-	node *temp = mynode;
-	while (mynode) {
-		temp = mynode->next;
-		free(mynode);
-		mynode = temp;
-	}
-	// set pointers to NULL
-	temp = NULL;
-	mynode = NULL;
-}
-
-/* Frees all nodes and then the wrapper for the queue*/
+/* Frees all nodes in the LinkList and then frees the wrapper for the LinkList*/
 void destroy_list(linklist * mylist)
 {
-	if (NULL == mylist) {
-		fprintf(stderr, "Cannot destroy linklist.\n");
+	if (NULL == mylist) // check to see if the list exists
+    {
+		fprintf(stderr, "Cannot destroy linklist.\n"); // if it doesnt print to stderr and leave func
 		return;
 	}
-	// free all my nodes in the queue
-	destroy_node(mylist->head);
+	node *my_node = mylist->head; // set a pointer to the head node of the linklist(LL)
+	while (my_node) // while that node is not NULL
+    {
+		node *temp = my_node -> next; // create a pointer to the next node in the LL
+		if (mylist->delete) // if a delete function exists
+        {
+			mylist->delete(temp->data); // call the delete function on the next node
+		}
+		free(temp); // then free that node.
+	}
+	free(mylist); // then free the wrapper
+}
 
-	// then free the wrapper
-	free(mylist);
-	mylist = NULL;
+/* Search the link list in O(N) time. Must provide a search function that has a
+ void pointer as a parameter and returns an integer. If search returns a 1, the
+item will be returned, else, move forwards to next node. If no nodes match or 
+there is an error, NULL is returned. */
+void *search(linklist * queue, search_f search (void *))
+{	
+	if (NULL == queue || NULL == search)  // check pointers and check the search function
+    {
+		fprintf(stderr, "Cannot search due to invalid pointer.\n");
+		return NULL;
+	}
+	
+	if (queue->head == NULL)  // if the head is == NULL return false, theres nothing.
+    {
+		return NULL;
+	}
+	
+	
+	node *head = queue->head; // set head to the queue head
+	while (head != NULL)  // While the head is not null
+    {  
+		void * head_data = head->data; // typecast the data in the Node to a void pointer
+		if (search(head_data) == 1) // if the search returns 1, it is successful
+		{
+			return head_data; // return that item
+		}
+		head = head->next; // otherwise move forward a node.
+	}
+	return NULL; // If there is no match, then return NULL
+}
 
+/* NOP Function, does nothing and returns nothing */
+void nop_delete_func(void *item)
+{
+	(void)item;
+}
+
+/*Purge list keeps the linklist allocation, but removes all elements and frees
+ all elements while keeping the wrapper. */
+void purge_list(linklist * mylist)
+{
+	if (NULL == mylist) // if the linklist pointer is NULL
+    {
+		fprintf(stderr, "Cannot destroy linklist.\n"); // Call fprint F and leave the function
+		return;
+	}
+	node *mynode = mylist->head; // Set a pointer to the head
+	while (mynode) // while the head is not null
+    {
+		node *temp = mynode->next; // create a node pointer that holds the next item in the linklist
+		free(mynode); // free the current item
+		mynode = temp; // set the current item to the next item and go back to start of loop
+	}
+	mylist->count = 0; // set the count of items to zero
+	mylist->head = NULL; // set the head to NULL
+	mylist->tail = NULL; // set the tail to NULL
+}
+
+/* Check to see if the linklist is empty. If there is an error, -1 is returned.
+If the linklist is empty then 1 is returned, if not empty, zero is returned. */
+int is_empty(linklist * list)
+{
+	if (NULL == list) // if the linklist is NULL
+    {
+		fprintf(stderr, "List is null.\n");  // Print to stderr and return -1 for error.
+		return -1;
+	}
+	if (0 == sizeoflist(list)) // call sizeoflist that returned the count of elements in a list
+    {
+		return 1; // if there is 0 elements, then return 1 for True
+	} else {
+		return 0; // if there is less than or more that 0 elements, return 0 for False
+	  }
+
+	return -1; // If the code made it here, there is an error that is undefined.
 }
