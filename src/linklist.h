422eaef765e6545aadab16350f0460a204d724a9
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-06-08 07:49:08 -0400

    Update the prototype of Search

--- src/linklist.h
+++ src/linklist.h
@@ -1,8 +1,12 @@
 #ifndef LINKLIST_H
 #define LINKLIST_H
 
+
+typedef void (*del_f)(void *data);
+typedef int (*search_f)(void * data);
+
 typedef struct linknode {
-	char *word;
+	void *data;
 	struct linknode *next;
 
 } node;
@@ -10,30 +14,36 @@ typedef struct linknode {
 typedef struct Linklist {
 	node *tail;
 	node *head;
+        del_f delete;
 	int count;
 } linklist;
 
+
+
 /*Create links makes a linked list with no nodes. It holds the properties
 of a queue.  Insert will instead create the individual node when inserting.*/
-linklist *create_links(void);
+linklist *create_links(del_f del);
+
+int is_empty(linklist * list);
 
 /* Create an individual node that assigns the word passed into it, as the word in the node.
 The node will be inserted at the tail of all the structures.*/
-linklist *enqueue(linklist * mylist, char *word);
+linklist *enqueue(linklist * mylist, void * data);
 
 // Print list prints all objects that are currently in the list
 void print_list(linklist * mylist);
 
 /* pop returns the first object that is at the head and moves the head forward*/
-node * pop(linklist * my_list);
+void * pop(linklist * my_list);
 
 /* Peek is a void function that allows you to see what the head holds,
 and if there is a next value then it will show that value*/
-void peek(linklist * mylist);
+void *peek(linklist * mylist);
 
+void nop_delete_func(void *item);
 
 /*Dequeue behaves exactly like pop*/
-node * dequeue(linklist * mylist);
+void * dequeue(linklist * mylist);
 
 /* Return size of my list*/
 int sizeoflist(linklist * mylist);
@@ -41,4 +51,7 @@ int sizeoflist(linklist * mylist);
 void push (linklist * mylist, char * word);
 /* Frees all nodes and then the wrapper for the queue*/
 void destroy_list(linklist * mylist);
+void purge_list (linklist * mylist);
+
+void* search(linklist * queue, search_f search (void *));
 #endif
